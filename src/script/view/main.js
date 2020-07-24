f4d55ee1635db31239cd559e44e2087876a949b4
Adi Purnomo <adipurnomo811@gmail.com>
2020-07-24 19:20:42 +0700

    replace #search-bar to selector search-bar

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,7 +1,8 @@
+import "../component/search-bar.js";
 import DataSource from "../data/data-source.js";
 
 const main = () => {
-    const searchElement = document.querySelector("#search-bar");
+    const searchElement = document.querySelector("search-bar");
     const clubListElement = document.querySelector("#clubList");
 
     const onButtonSearchClicked = async () => {
