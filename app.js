6deb9fcdee1366d4b087b1c0c2d69c88aa1ff5df
Adi Purnomo <adipurnomo811@gmail.com>
2020-07-24 18:45:02 +0700

    change tag app-bar and import to app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // import main from ".../src/script/view/main.js";
+import "./src/script/component/app-bar";
 import main from "./src/script/view/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
